ea5d417e64891d6040cbebb26d2262922d250f4c
Jeff Kaplan <jkaplan049@gmail.com>
2020-11-12 22:02:51 -0500

    Homework

---
+++ JavascriptNov2020/JavascriptLibrary/05-types.js
@@ -0,0 +1,15 @@
+/*
+TYPES
+
+1-grammerAndTypes
+    05-types.js
+/*
+
+//Boolean
+var on = true;
+console.lon(on);
+
+let off = false;
+console.log(off);
+
+//boolean can represent: true//false, yes/no, on/off
---
+++ JavascriptNov2020/JavascriptLibrary/1-GrammerAndTypes/01-comments-and-console.js
@@ -0,0 +1,47 @@
+/*
+Comments
+
+1-grammerAndTypes
+    01-comments-and-console.js
+*/
+
+//value of a comment:
+//communicates to anyone reading code
+
+//types of comments:
+//1) single-line comments
+
+/*
+J
+E
+F
+F
+2) Multi-line
+*/
+
+//console.log
+//outputs some message to the console
+console.log('this is a console.log message!');
+console.log('you can seperate parts of a console message with commas,', 'just like this')
+
+console.log(13,15,17,19);
+
+//console.log and debugging
+//we can debug with console.log messages in 2 big ways
+//1) we can check the status of some javascript 'thing"
+//2) we can check a process we have built with repeated console.log messages
+
+//example 1:
+var sampleName = 'Jeff'
+console.log(sampleName);
+
+//example 2:
+var sampleNum = 1;
+console.log(sampleNum);
+Samplenum++;
+console.log(sampleNum);
+Samplenum++;
+console.log(sampleNum);
+
+
+
